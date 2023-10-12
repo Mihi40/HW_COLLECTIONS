21a87f1ce66bd0cfecc2170d83c24d4f5f8da582
mih sve <RMS4083@yandex.ru>
2023-10-12 17:02:42 +0300

    ШвBranch Fast changed

--- src/main/java/ru/netology/qa/Game.java
+++ src/main/java/ru/netology/qa/Game.java
@@ -1,24 +1,23 @@
 package ru.netology.qa;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
 
 public class Game {
-    static List<Player> players = new ArrayList<>();
+    static HashMap<String, Player> map = new HashMap<>();
 
-    public static void register(Player player) {
-        players.add(player);
-    }
+    public static void register(String key,Player player) {
+     map.put(key,player);
+   }
 
     public static int round(String playerName1, String playerName2) {
         Player player1 = null;
         Player player2 = null;
-        for (Player player : players) {
-            if (player.getName().equals(playerName1)) {
-                player1 = player;
+        for (String key : map.keySet()) {
+            if (key.equals(playerName1)) {
+                player1 = map.get(key);
             }
-            if (player.getName().equals(playerName2)) {
-                player2 = player;
+            if (key.equals(playerName2)) {
+                player2 = map.get(key);
             }
         }
         if (player1 == null) {
